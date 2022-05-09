18acf66d5f68a78f9ec7d61210ad26ecdeb68065
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-05-09 12:43:51 -0400

    added userprompt to ask questions also added connection to js file functinos

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const inquirer = require("inquirer");
 //for connection
 require('dotenv').config();
 
+const {  
+  viewAllDepartments,
+  viewAllRoles,
+  viewAllEmployees,
+  addADepartment,
+  addARole,
+
+} = require('./lib/function');
 
 const connection = mysql.createConnection({
     host: process.env.HOST,
@@ -19,10 +27,67 @@ const connection = mysql.createConnection({
     }
     console.log("\nConnected to Database\n");
     //add function to execute when server starts 
+    userPrompt();
 });
 
-//first question should be given options 
-//each will have its own function 
+//questions
+
+const userPrompt = () => {
+  console.log(
+    "🅆🄴🄻🄲🄾🄼🄴 🅃🄾 🅃🄷🄴 🄴🄼🄿🄻🄾🅈🄴🄴 🅃🅁🄰🄲🄺🄴🅁"
+
+    
+  );
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "questions",
+        message: "What would you like to do?",
+        choices: [
+          "View all Departments",
+          "Add a Department",
+          "View all Roles",
+          "Add a Role",
+          "View all Employees",
+          "Add an Employee", //need to add 
+          "Update Employee role",
+          "Exit Employee Tracker",
+        ],
+        loop: false,
+        pageSize: 15,
+      },
+    ])
+    .then((answers) => {
+      options = answers.questions;
+
+      if (options === "View all Departments") {
+        viewAllDepartments();
+      }
+      if (options === "Add a Department") {
+        addADepartment();
+      }
+
+
+      if (options === "View all Roles") {
+        viewAllRoles();
+      }
+
+      if (options === "Add a Role") {
+        addARole();
+      }
+
+      if (options === "View all Employees") {
+        viewAllEmployees();
+      }
+
+      if (options === "Exit Employee Tracker") {
+        console.log("BYE!!")
+        console.log("press ctrl C to end")
+        connection.break; //not sure yet y it does not break and exit 
+      }
+    });
+};
 
 //view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
   
